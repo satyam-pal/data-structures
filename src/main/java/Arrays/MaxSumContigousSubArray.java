4092ee24915bbb89f6f649ff874a5f08b142d68a
satyam-pal <git@7417>
2023-06-29 15:45:28 +0530

    Added Max Sum Contiguous Subarray

---
+++ src/main/java/Arrays/MaxSumContigousSubArray.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class MaxSumContigousSubArray {
+    /*
+    Problem Description:
+    Find the maximum sum of contiguous non-empty subarray within an array A of length N.
+
+     */
+    public int maxSubArray(final int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int n = nums.length;
+        int currentSum = 0;
+        for(int i = 0; i < n; i++){
+            currentSum += nums[i];
+            if(maxSum < currentSum){
+                maxSum = currentSum;
+            }
+            if(currentSum < 0)
+                currentSum = 0;
+        }
+        return maxSum;
+    }
+}
---
+++ src/test/java/Arrays/MaxSumContigousSubArrayTest.java
@@ -0,0 +1,49 @@
+package Arrays;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class MaxSumContigousSubArrayTest {
+        MaxSumContigousSubArray maxSub = new MaxSumContigousSubArray();
+        @Test
+        public void testMaxSubArray_PositiveNumbers() {
+            int[] arr = {1, 2, 3, 4, 5};
+            int expected = 15;
+            int actual = maxSub.maxSubArray(arr);
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void testMaxSubArray_NegativeNumbers() {
+            int[] arr = {-1, -2, -3, -4, -5};
+            int expected = -1;
+            int actual = maxSub.maxSubArray(arr);
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void testMaxSubArray_MixedNumbers() {
+            int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+            int expected = 6;
+            int actual = maxSub.maxSubArray(arr);
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void testMaxSubArray_SingleElement() {
+            int[] arr = {7};
+            int expected = 7;
+            int actual = maxSub.maxSubArray(arr);
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void testMaxSubArray_EmptyArray() {
+            int[] arr = {};
+            int expected = Integer.MIN_VALUE;
+            int actual = maxSub.maxSubArray(arr);
+            assertEquals(expected, actual);
+        }
+
+
+}
